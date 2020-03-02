5a2c61d0dc35925e230d75b8439e2ec52189e090
mohsen shoraki <mohsen.shoraki@gmail.com>
2020-03-02 01:41:06 -0600

    initial push.

---
+++ src/main/java/com/capital/sumo/MindSumo/MindSumoApplication.java
@@ -0,0 +1,13 @@
+package com.capital.sumo.MindSumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindSumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindSumoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capital/sumo/MindSumo/MindSumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capital.sumo.MindSumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindSumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
